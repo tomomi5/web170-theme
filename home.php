84ce01318460a2dac7b73e08276a2c3477b83741
tomomi5 <tomomi.523@gmail.com>
2017-04-26 14:15:20 -0700

    first

---
+++ home.php
@@ -0,0 +1,13 @@
+<?php include("includes/top.php"); ?>
+    
+    <div id="main">
+        
+        <blockquote>Every flower is a soul blossoming in nature.</blockquote>
+    
+        <img rel="flower1" src="images/flower1.jpg"  class="main"/>
+        
+    </div>
+
+<?php include("includes/bottom.php"); ?>
+
+    
---
+++ includes/bottom.php
@@ -0,0 +1,16 @@
+<footer>
+    ©2017 | All Rights Reserved | Tomomi Matsuzaki
+    </footer>
+    
+    <script>
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+</script>
+</body>
+</html>
