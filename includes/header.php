d12f425165445c7c5317e32f4fa24610d087ad8c
tomomi5 <tomomi.523@gmail.com>
2017-05-10 20:02:23 -0700

    /Volumes/FLASH DRIVE/spring2017/web170/web170_theme/functions.php /Volumes/FLASH DRIVE/spring2017/web170/web170_theme/images /Volumes/FLASH DRIVE/spring2017/web170/web170_theme/includes /Volumes/FLASH DRIVE/spring2017/web170/web170_theme/index.php /Volumes/FLASH DRIVE/spring2017/web170/web170_theme/main.php /Volumes/FLASH DRIVE/spring2017/web170/web170_theme/screenshot.png /Volumes/FLASH DRIVE/spring2017/web170/web170_theme/style.css

---
+++ includes/header.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+    
+<head>
+    <title>KARIN Flower Arrangement</title>
+    <meta charset= "UTF-8">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    
+</head>
+    
+    <!-- ----------------BODY------------------ -->   
+<body>
+    
+    <header>
+    <!-- <h1 class="title"><a href = "home.php">- KARIN -</a></h1> -->
+        <a href = "home.php"><img alt="Logo" src="images/karinlogo.svg" id="logo" width="220px"/></a>
+        
+     <!-- NAVIGATION -->
+        
+    <nav>
+        <ul class="topnav" id="myTopnav">
+            <li><a href = "home.php">Home</a></li>
+            <li><a href = "main.php"><li>About</a></li>
+            <li><a href = "">Service</a></li>
+            <li><a href = "">Contact</a></li>
+            <li class="icon"><a href="javascript:void(0);" onclick="myFunction()">&#9776;</a></li>
+        </ul>
+    </nav>
+        
+    <!-- NAVIGATION END -->
+        
+    </header>
+    
